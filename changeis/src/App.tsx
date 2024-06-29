9cb482edc8c9c9bf75486b7dbd77a74b645c30f4
Roland Forbes <rolandandewforbes@gmail.com>
2024-06-28 22:25:24 -0600

    Continuing to iterate on the main landing screen for the ChangeIs FakerAPI coding challenge. Trying to make this front end look as clean as possible on this rainy Friday evenin'..

--- changeis/src/App.tsx
+++ changeis/src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import changeIsLogo from '/changeIsLogo.png';
+import reactLogo from './assets/react.svg';
+import typescriptLogo from './assets/typescript.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='align-items-center d-flex'>
+      <div className='align-items-center d-flex gap-2'>
         <div>
-          <img src={changeIsLogo} className="logo" alt="Vite logo" />
+          <img src={changeIsLogo} className="logo" alt='changeis logo' />
         </div>
-        <h2 className='opacity-50'>
-          &
-        </h2>
-        <div>
-          <h1 className='fw-light'>
-            ROLAND FORBES
-          </h1>
-        </div>
+        <h2 className='brown'>&</h2>
+        <h1 className='fw-light'>
+          ROLAND FORBES
+        </h1>
       </div>
-      <h3 className='mb-0 opacity-50 text-start'>
+      <h3 className='mb-1 opacity-50 text-start'>
         <u>FAKER API CODING CHALLENGE SUBMISSION</u>
       </h3>
-      <h5 className='m-0 opacity-25 text-start'>
-        Built using ReactJS, Typescript, && Vite
+      <h5 className='align-items-center d-flex gap-1 logo-xs m-0 text-start'>
+        <span className='brown pe-2'>
+          <u>BUILT USING:</u>
+        </span>
+        <img src={reactLogo} />
+        <img src={typescriptLogo} />
+        <img src={viteLogo} />
       </h5>
     </>
   )
