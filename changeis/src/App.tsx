1ba795d87753ecb2b4b7a303c5d5cb892369bf00
Roland Forbes <rolandandewforbes@gmail.com>
2024-06-28 22:34:15 -0600

    Wrapping the three logos for the 'Built Using' section in their own <a /> tags, with clickable links directly to their corresponding documentations. Making it so that just the react logo spins (kind of a cool little animation I think, shows some css skills subtly maybe?).

--- changeis/src/App.tsx
+++ changeis/src/App.tsx
@@ -25,9 +25,15 @@ function App() {
         <span className='brown pe-2'>
           <u>BUILT USING:</u>
         </span>
-        <img src={reactLogo} />
-        <img src={typescriptLogo} />
-        <img src={viteLogo} />
+        <a href='https://react.dev' target='_blank'>
+          <img className='logo-spin' src={reactLogo} />
+        </a>
+        <a href='https://typescriptlang.org' target='_blank'>
+          <img src={typescriptLogo} />
+        </a>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} />
+        </a>
       </h5>
     </>
   )
