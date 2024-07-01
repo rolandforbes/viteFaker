0fabb88f1626ed26003e0ac9d2076dece6e56138
Roland Forbes <rolandandewforbes@gmail.com>
2024-06-30 20:39:42 -0600

    Another firebase deploy bundle change and some more syntactically proper implementation of Typescript throughout.

--- changeis/src/App.tsx
+++ changeis/src/App.tsx
@@ -3,8 +3,9 @@ import FakerAPIDemo from './components/FakerAPIDemo';
 import HomePage from './components/HomePage';
 import './App.css';
 
+// This is an app that calls FakerAPI with the data type chosen by currentUser.
 const App = () => {
-  const [showFakerAPI, setShowFakerAPI] = useState(false);
+  const [showFakerAPI, setShowFakerAPI] = useState<boolean>(false);
   return <>
     <HomePage setShowFakerAPI={setShowFakerAPI} showFakerAPI={showFakerAPI}>
       <FakerAPIDemo showFakerAPI={showFakerAPI} />
